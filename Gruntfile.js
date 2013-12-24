0066386e6929d36795322da50db4e832c856e42a
Marat Abdullin <dakota@brokenpipe.ru>
2013-12-24 05:07:40 +0400

    Build scripts.

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            all: {
+                src: ['src/**/*.js', 'Gruntfile.js'],
+                options: {
+                    jshintrc: '.jshintrc'
+                }
+            }
+        },
+
+        concat: {
+            dist: {
+                src: [
+                    'src/core.js',
+                    'src/**/*.js'
+                ],
+                dest: 'coo-coo.js'
+            }
+        },
+
+        qunit: {
+            all: ['test/**/*.html']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+
+    grunt.registerTask('default', ['jshint', 'concat'/*, 'qunit'*/]);
+};
